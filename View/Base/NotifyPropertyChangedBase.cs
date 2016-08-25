4c555bd94e1360f34a9ea1afac819dae335c7974
南出 和宏 <k-minamide@digital-face.net>
2016-08-26 08:05:16 +0900

    test

---
+++ View/Base/NotifyPropertyChangedBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+
+namespace ComprehensionCheck.Base
+{
+    public class NotifyPropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propartyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propartyName));
+            }
+        }
+    }
+}
---
+++ View/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using ComprehensionCheck.Base;
+
+namespace ComprehensionCheck.ViewModel
+{
+    public class MainViewModel : NotifyPropertyChangedBase
+    {
+        private DataTable data = new DataTable();
+        public DataTable Data
+        {
+            get
+            {
+                return this.data;
+            }
+
+            set
+            {
+                this.data = value;
+                OnPropertyChanged("Data");
+            }
+        }
+    }
+}
